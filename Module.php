cd81083e88327a050abbd348d453d5d9e49f4258
Вадим <guts.vadim@gmail.com>
2016-03-23 15:52:13 +0200

    default rout language

---
+++ Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace bl\cms\language;
+
+/**
+ * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ */
+class Module extends \yii\base\Module
+{
+    public $defaultRoute = 'language';
+}
